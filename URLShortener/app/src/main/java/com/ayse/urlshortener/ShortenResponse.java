b129b040f5566b60a8bcd60c86f5927791226db2
ayseakpnr <ayseeakpnr.2002@gmail.com>
2024-12-04 11:35:59 +0300

    Simple_Social_Media_App_with_Firebase

---
+++ URLShortener/app/src/main/java/com/ayse/urlshortener/ShortenResponse.java
@@ -0,0 +1,16 @@
+package com.ayse.urlshortener;
+
+public class ShortenResponse {
+
+    private String link;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
+
+
